919a515d6beb2af007c2ccf564e51d83fa7e25e4
Fabian144 <fabianyousif24@gmail.com>
2025-11-05 14:46:35 +0100

    Added the cypress test

---
+++ cypress/e2e/ratingSystem.cy.js
@@ -0,0 +1,40 @@
+describe("Rating system", () => {
+  beforeEach(() => {
+    cy.visit("/recipePage.html");
+
+    cy.window().then((win) => {
+      cy.spy(win.console, "log").as("consoleLog");
+    });
+  });
+
+  it("Does not update the average rating when submitting without a comment", () => {
+    cy.get(".recipe-star-container i").first().click();
+
+    cy.get("#addComment").click();
+
+    cy.get('@consoleLog').its('callCount').should('be.at.most', 0)
+  });
+
+  it("Updates the average rating only when a comment is added", () => {
+    cy.get(".recipe-star-container i").first().click();
+
+    cy.get("#userName").type("Cypress Tester");
+    cy.get("#userComment").type("Tasty!");
+    cy.get("#addComment").click();
+
+    cy.get("@consoleLog").should("have.callCount", 1);
+
+    cy.get("#addComment").click();
+    cy.get("@consoleLog").should("have.callCount", 1);
+  });
+});
+
+// För att kunna köra testet:
+//
+// Kör först kommandot npm install
+// Behövs i funktionen som lägger till ett nytt omdöme (addNewRating()):
+//  console.log(
+// 					"This vote: " + this.addedVote,
+//           "New average rating: " + recipe.rating[0].current_stars,
+//           "New total votes: " + recipe.rating[1].total_votes
+//         );
