d1e201def51f8eb778fedbac0719b8a8625b9260
Fabian144 <fabianyousif24@gmail.com>
2025-11-12 21:39:23 +0100

    Remove test menu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <nav class="menu"></nav>
   <router-view></router-view>
 </template>
 
 <script></script>
 
-<style scoped>
-@media (min-width: 992px) {
-  .menu {
-    height: 100dvh;
-    width: 15em;
-    background-color: black;
-    float: left;
-  }
-}
-</style>
+<style scoped></style>
