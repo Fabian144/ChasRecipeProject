1a41447675404ccac102c7522076b69df8798bf0
Fabian144 <fabianyousif24@gmail.com>
2025-11-19 21:47:13 +0100

    Add back initial data fetch module

--- src/modules/fetchRecipeData.js
+++ src/modules/fetchRecipeData.js
@@ -1,4 +1,21 @@
-export default async function fetchAllRecipes() {
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch failed:', error);
+  }
+}
+
+const data = await fetchData(`../src/data/recept.json`);
+
+const recipes = data.recipes;
+
+export default recipes;
+
+export async function fetchAllRecipes() {
   try {
     const response = await fetch(
       'REMOVED/REMOVED/recipes',
