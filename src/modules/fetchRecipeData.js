558abda784ccd419ed6ef94d385c77a86650f722
Fabian144 <fabianyousif24@gmail.com>
2025-11-19 16:01:47 +0100

    Test fetch API and recipe card adaptation

--- src/modules/fetchRecipeData.js
+++ src/modules/fetchRecipeData.js
@@ -9,7 +9,9 @@ async function fetchData(url) {
   }
 }
 
-const data = await fetchData(`../src/data/recept.json`);
+const data = await fetchData(
+  `https://recipes.bocs.se/api/v1/c8d9e0f1-a2b3-4c5d-6e7f-8a9b0c1d2e3f/recipes/637c639a-1d94-43be-a347-8076dd69eb82`,
+);
 
 const recipes = data.recipes;
 
