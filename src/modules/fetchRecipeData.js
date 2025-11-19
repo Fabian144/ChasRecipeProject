b969da72d383a7a9ffe47c966b51f5114fa8ed74
Fabian144 <fabianyousif24@gmail.com>
2025-11-19 21:47:13 +0100

    Add back initial data fetch module

--- src/modules/fetchRecipeData.js
+++ src/modules/fetchRecipeData.js
@@ -1,4 +1,21 @@
-export default async function fetchAllRecipes() {
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch failed:', error);
+  }
+}
+
+const data = await fetchData(`../src/data/recept.json`);
+
+const recipes = data.recipes;
+
+export default recipes;
+
+export async function fetchAllRecipes() {
   try {
     const response = await fetch(
       'https://recipes.bocs.se/api/v1/c8d9e0f1-a2b3-4c5d-6e7f-8a9b0c1d2e3f/recipes',
