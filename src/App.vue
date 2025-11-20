b51f456b1ea088bf611a1c74dc8fd16b2775597f
Fabian144 <fabianyousif24@gmail.com>
2025-11-20 21:24:44 +0100

    Fix comment still adding on fetch fail

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,20 @@
 body {
   margin: 0;
 }
+
+.loading {
+  width: 150px;
+  animation: spin 2s linear infinite;
+  margin: auto;
+  display: flex;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
