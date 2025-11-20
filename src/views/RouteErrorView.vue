f31b6a7cb429d38b0424ab0ec1b1f3ce21611ca5
Fabian144 <fabianyousif24@gmail.com>
2025-11-20 21:51:36 +0100

    Add 404 page when route not found

---
+++ src/views/RouteErrorView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-found">
+    <h1>404 – Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.not-found {
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+</style>
