fff1bc1e4672c30f3eb39c480d3315d6f9f2cdff
Fabian144 <fabianyousif24@gmail.com>
2025-11-20 21:51:36 +0100

    Add 404 page when route not found

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const HomeView = () => import('../views/HomeView.vue');
 const RecipeView = () => import('../views/RecipeView.vue');
+const RouteError = () => import('../views/RouteErrorView.vue');
 
 let routes = [
   { path: '/', component: HomeView },
   { path: '/recipe/:recipeId', name: 'theRecipe', component: RecipeView },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'RouteError',
+    component: RouteError,
+  },
 ];
 
 const router = createRouter({
