47e5fb65eb1f1e623467e7cfb7020f430269e80d
Fabian144 <fabianyousif24@gmail.com>
2025-11-22 12:20:58 +0100

    Add star display component for reusability

---
+++ src/components/StarDisplay.vue
@@ -0,0 +1,42 @@
+<template>
+  <font-awesome-icon
+    v-for="starIcon in starIcons"
+    :icon="starIcon.value <= Math.round(ratingValue) ? 'fa-solid fa-star' : 'fa-regular fa-star'"
+  />
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, far, faStar);
+
+export default {
+  components: {
+    FontAwesomeIcon,
+  },
+
+  props: {
+    ratingValue: {
+      type: Number,
+    },
+  },
+
+  data() {
+    return {
+      starIcons: [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }, { value: 5 }],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.fa-star {
+  width: 100%;
+	height: 100%;
+}
+</style>
