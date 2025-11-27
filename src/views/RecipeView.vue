add8abf2304e29399c4582fbef9764db6f36a12f
Fabian144 <fabianyousif24@gmail.com>
2025-11-27 17:18:30 +0100

    Move loading icon to separate component

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -7,14 +7,7 @@
     <a href="/">Tillbaka till startsidan</a>
   </div>
 
-  <div class="loading-container" v-if="loading">
-    <img
-      src="../images/e50c7e38-b24e-4fbd-a400-516909b77df5.png"
-      alt="Julgran"
-      class="loading-icon"
-    />
-    <p class="loading-text">Laddar...</p>
-  </div>
+  <LoadingIcon v-if="loading"></LoadingIcon>
 
   <div v-else>
     <RatingSection :recipe-id="recipeId" />
@@ -23,6 +16,7 @@
 </template>
 
 <script>
+import LoadingIcon from '../components/LoadingIcon.vue';
 import RatingSection from '../components/recipepage/RatingSection.vue';
 import CommentSection from '../components/recipepage/commentSection.vue';
 
@@ -30,6 +24,7 @@ export default {
   components: {
     CommentSection,
     RatingSection,
+    LoadingIcon,
   },
 
   data() {
