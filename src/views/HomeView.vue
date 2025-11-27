47d07a653cfcca8c969a485810501781bfc3cb40
Fabian144 <fabianyousif24@gmail.com>
2025-11-27 17:45:09 +0100

    Stop loading with "finally" in fetch async function and fix not showing only error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
+  <LoadingIcon v-if="loading"></LoadingIcon>
+
   <FetchError
-    v-if="errorMessage"
+    v-else-if="errorMessage"
     :error-message="errorMessage"
     :error-status="errorStatus"
   ></FetchError>
 
-  <LoadingIcon v-if="loading"></LoadingIcon>
-
-  <div v-else>
+  <template v-else>
     <main class="recipe-card-section">
       <RecipeCardSection :recipes="recipes" />
     </main>
-  </div>
+  </template>
 </template>
 
 <script>
@@ -41,15 +41,15 @@ export default {
         'https://recipes.bocs.se/api/v1/c8d9e0f1-a2b3-4c5d-6e7f-8a9b0c1d2e3f/recipes',
       );
       if (!response.ok) {
-        this.loading = false;
         this.errorMessage = `Recepten kunde inte laddas in eller hittas inte`;
         this.errorStatus = `${response.status}`;
         throw new Error(`Status: ${response.status}`);
       }
       this.recipes = await response.json();
-      this.loading = false;
     } catch (error) {
       console.error('Fetch failed:', error);
+    } finally {
+      this.loading = false;
     }
   },
 };
