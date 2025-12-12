db3db8a7704c0a5a76caceef59b618cb78113ab0
Fabian144 <fabianyousif24@gmail.com>
2025-12-12 21:17:04 +0100

    Format recipe card section, sidebar and rating section component

--- src/components/homepage/RecipeCardSection.vue
+++ src/components/homepage/RecipeCardSection.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="recipe-cards-container">
-    <router-link v-for="recipe in recipes" :key="recipe.id" :to="{ name: 'theRecipe', params: { recipeId: recipe.id } }"
-      :aria-label="`Länk till ${recipe.title} receptet`">
-      <RecipeCard :recipe-image="recipe.imageUrl" :recipe-name="recipe.title" :recipe-description="recipe.description"
-        :recipe-ingredients="recipe.ingredients" :recipe-cooking-time="recipe.timeInMins"
-        :recipe-average-rating="calculateAverage(recipe.ratings)" :amount-of-ratings="recipe.ratings.length" />
+    <router-link
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :to="{ name: 'theRecipe', params: { recipeId: recipe.id } }"
+      :aria-label="`Länk till ${recipe.title} receptet`"
+    >
+      <RecipeCard
+        :recipe-image="recipe.imageUrl"
+        :recipe-name="recipe.title"
+        :recipe-description="recipe.description"
+        :recipe-ingredients="recipe.ingredients"
+        :recipe-cooking-time="recipe.timeInMins"
+        :recipe-average-rating="calculateAverage(recipe.ratings)"
+        :amount-of-ratings="recipe.ratings.length"
+      />
     </router-link>
   </div>
 </template>
@@ -57,10 +67,10 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-		gap: 1em;
+    gap: 1em;
   }
 
-  .recipe-cards-container>a {
+  .recipe-cards-container > a {
     width: 100%;
   }
 }
@@ -69,7 +79,7 @@ export default {
   .recipe-cards-container {
     display: grid;
     grid-template-columns: 1fr 1fr;
-		gap: 0.75em;
+    gap: 0.75em;
   }
 }
 
