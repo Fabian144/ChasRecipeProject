93c9b544b24add51a6a204b27ea4f72291c87d96
Fabian144 <fabianyousif24@gmail.com>
2026-01-13 00:04:02 +0100

    Fix base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/ChasRecipeProject",
+	base: '/ChasRecipeProject/',
   plugins: [
     vue(),
     vueDevTools(),
